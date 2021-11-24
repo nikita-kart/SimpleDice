59af0ece89582f3fb33aff0f5b0694197a64f2d8
Nikita Kartashov <kartashov.nm@gmail.com>
2021-11-24 20:37:45 +0700

    Добавьте файлы проекта.

---
+++ Demo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using SimpleDice;
+
+namespace Demo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DiceSet _d20 = new DiceSet(1, 20);
+			DiceSet _3d6 = new DiceSet(3, 6);
+			DiceSet _10d2 = new DiceSet(10, 2);
+
+			Console.WriteLine(_d20.ToString());
+			Console.WriteLine();
+			for (int i = 0; i < 5; i++)
+				Console.WriteLine(_d20.Roll());
+			Console.WriteLine("====================\n");
+
+			int result;
+			int[] rolls;
+
+			Console.WriteLine(_3d6.ToString());
+			Console.WriteLine();
+			result = _3d6.Roll(out rolls);
+			Console.WriteLine($"{result} ({string.Join(", ", rolls)})");
+			Console.WriteLine(_3d6.Average() + " " + _3d6.Average(true));
+			Console.WriteLine("====================\n");
+
+			Console.WriteLine(_10d2.ToString("x"));
+			Console.WriteLine();
+			Console.WriteLine($"{_10d2.Min} - {_10d2.Max}");
+			for (int i = 0; i < 5; i++)
+				Console.WriteLine(DiceSet.Roll(_10d2));
+			Console.WriteLine("\n");
+
+			Console.WriteLine("Press any key to exit");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ SimpleDice/DiceSet.cs
@@ -0,0 +1,133 @@
+﻿using System;
+
+namespace SimpleDice
+{
+	/// <summary>
+	/// A simple class to handle NdM sets of dices
+	/// </summary>
+	public class DiceSet
+	{
+		private Random random;
+
+		/// <summary>
+		/// Number of dices in this dice set
+		/// </summary>
+		public int Count { get; private set; }
+		/// <summary>
+		/// Number of sides of each dice in this dice set
+		/// </summary>
+		public int Sides { get; private set; }
+
+		/// <summary>
+		/// Minimum possible outcome
+		/// </summary>
+		public int Min { get { return Count; } }
+		/// <summary>
+		/// Maximum possible outcome
+		/// </summary>
+		public int Max { get { return Count * Sides; } }
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="_count">Number of dices in this set. Must be 1 or greater</param>
+		/// <param name="_sides">Number of sides each dice have. Must be 2 or greater</param>
+		public DiceSet(int _count = 1, int _sides = 6)
+		{
+			ChangeSet(_count, _sides);
+			random = new Random();
+		}
+
+		/// <summary>
+		/// Change count of dices in this set and number of their sides
+		/// </summary>
+		/// <param name="_count">Number of dices in this set. Must be 1 or greater</param>
+		/// <param name="_sides">Number of sides each dice have. Must be 2 or greater</param>
+		public void ChangeSet(int _count, int _sides)
+		{
+			if (_count < 1)
+				_count = 1;
+			if (_sides < 2)
+				_sides = 0;
+
+			Count = _count;
+			Sides = _sides;
+		}
+
+		/// <summary>
+		/// Converts this dice set into its string representation for purposes like displaying in interface
+		/// </summary>
+		/// <param name="separator">Separator symbol or string between dice count and number of sides</param>
+		/// <returns>A string representation of this dice set</returns>
+		public string ToString(string separator = "d")
+		{
+			return $"{Count}{separator}{Sides}";
+		}
+
+		/// <summary>
+		/// Returns average result of rolling this dices, rounded down (default) or up if number is not integer
+		/// </summary>
+		/// <param name="roundUp">Is result must be rounded up?</param>
+		/// <returns>An average result of rolling this dice set</returns>
+		public int Average(bool roundUp = false)
+		{
+			float average = ((float)(Sides + 1) / 2) * Count;
+			if (roundUp)
+				return (int)Math.Ceiling(average);
+			return (int)Math.Floor(average);
+		}
+
+		/// <summary>
+		/// Roll the dices
+		/// </summary>
+		/// <returns>Result of a roll</returns>
+		public int Roll()
+		{
+			int result = 0;
+			for (int i = 0; i < Count; i++)
+			{
+				result+= random.Next(1, Sides + 1);
+			}
+			return result;
+		}
+
+		/// <summary>
+		/// Roll the dices and get each dice roll result
+		/// </summary>
+		/// <param name="rollsArray">Integer array which will contain individual roll results of each dice</param>
+		/// <returns>Result of a roll</returns>
+		public int Roll(out int[] rollsArray)
+		{
+			rollsArray = new int[Count];
+			int result = 0;
+			for (int i =0; i < Count; i++)
+			{
+				int roll = random.Next(1, Sides + 1);
+				result += roll;
+				rollsArray[i] = roll;
+			}
+			return result;
+		}
+
+		/// <summary>
+		/// Static method to roll any dice set
+		/// </summary>
+		/// <param name="diceSet">A dice set to roll</param>
+		/// <returns>Result of a dice set roll</returns>
+		public static int Roll(DiceSet diceSet)
+		{
+			return diceSet.Roll();
+		}
+
+		/// <summary>
+		/// Static method to roll any dice set and get each dice roll result
+		/// </summary>
+		/// <param name="diceSet">A dice set to roll</param>
+		/// <param name="rollsArray">Integer array which will contain individual roll results of each dice</param>
+		/// <returns>Result of a dice set roll</returns>
+		public static int Roll(DiceSet diceSet, out int[] rollsArray)
+		{
+			return diceSet.Roll(out rollsArray);
+		}
+	}
+}
